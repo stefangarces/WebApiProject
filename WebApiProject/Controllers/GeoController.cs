9233b037b86f27e13a42b2f101ba04d57ac957cc
Stefan Garces <stefangarces@gmail.com>
2021-04-29 14:43:57 +0200

    Started to work on the "GeoController" for the API.

---
+++ WebApiProject/Controllers/GeoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiProject.Controllers
+{
+    public class GeoController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetLatitude()
+        {
+            return null;
+        }
+        [HttpGet]
+        public ActionResult GetLongitude()
+        {
+            return null;
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult PostComment()
+        {
+            return null;
+        }
+    }
+}
